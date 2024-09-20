4f3b25b7dbf4b83af681e581674ee4b4bdfe29e5
Una <askoike@gmail.com>
2024-09-20 16:19:41 -0700

    5th attempt

--- cors-proxy/index.js
+++ cors-proxy/index.js
@@ -1,26 +1,18 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-// 啟用所有 CORS 請求
+const app = express();
 app.use(cors());
 
-// 處理所有 POST 請求
-app.use(express.json());
+app.use('/', createProxyMiddleware({
+    target: 'https://libretranslate.com',
+    changeOrigin: true,
+    pathRewrite: {
+        '^/': '', // 把所有請求轉發給 https://libretranslate.com
+    },
+}));
 
-app.post('*', (req, res) => {
-  const targetUrl = req.body.url;
-  fetch(targetUrl, {
-    method: req.body.method || 'GET',
-    headers: req.body.headers || {},
-    body: req.body.body ? JSON.stringify(req.body.body) : undefined,
-  })
-  .then(response => response.text())
-  .then(data => res.send(data))
-  .catch(error => res.status(500).send('Proxy error: ' + error.message));
-});
-
-// 伺服器監聽在3000端口
 app.listen(3000, () => {
-  console.log('CORS Proxy is running on port 3000');
+    console.log('CORS Proxy is running on port 3000');
 });
