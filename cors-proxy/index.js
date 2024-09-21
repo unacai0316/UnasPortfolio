54044b0f069473e2688f1651585c8afc5ce6bf34
Una <askoike@gmail.com>
2024-09-20 17:50:43 -0700

    change to direct request

--- cors-proxy/index.js
+++ cors-proxy/index.js
@@ -4,7 +4,7 @@ const axios = require('axios'); // 使用 axios 來發送請求
 
 const app = express();
 app.use(cors({
-    origin: 'https://d322-2600-1012-b00f-6960-a037-28a9-73b2-e8', // 確保沒有換行符
+    origin: 'https://d322-2600-1012-b00f-6960-a037-28a9-73b2-e8', // 確保這裡是你的前端網址
 }));
 
 app.use(express.json()); // 解析 JSON 請求體
@@ -14,8 +14,8 @@ app.post('/translate', async (req, res) => {
     const { q, source, target } = req.body;
 
     try {
-        // 發送請求到 CORS 代理的 LibreTranslate
-        const response = await axios.post('https://cors-anywhere.herokuapp.com/https://libretranslate.com/translate', {
+        // 直接發送請求到 LibreTranslate，不需要 CORS 代理
+        const response = await axios.post('https://libretranslate.com/translate', {
             q,
             source,
             target
