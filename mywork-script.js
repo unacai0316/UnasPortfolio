aac6bb3ee72c0fe38413834a475a27071ae5fe2d
Una <askoike@gmail.com>
2024-10-03 19:28:45 -0700

    added interval automatic img switch

--- mywork-script.js
+++ mywork-script.js
@@ -9,3 +9,35 @@ document.addEventListener('DOMContentLoaded', function() {
         document.querySelector('nav').classList.toggle('show');
     });
 });
+
+// Add image gallery anto functionality
+let images = [
+    'images/01.png', 'images/02.png', 'images/03.png', 
+    'images/04.png', 'images/05.png', 'images/06.png', 
+    'images/07.png', 'images/08.png'
+];
+
+let currentIndex = 0;
+let intervalId = setInterval(autoSwitchImage, 5000); // 每隔3秒切換一次圖片
+let isPlaying = true; // 判斷是否正在播放
+
+function showImage(src) {
+    document.getElementById('main-img').src = src;
+}
+
+function autoSwitchImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(images[currentIndex]);
+}
+
+function togglePlayPause() {
+    const btn = document.getElementById('playPauseBtn');
+    if (isPlaying) {
+        clearInterval(intervalId); // 暫停自動切換
+        btn.textContent = 'Play';
+    } else {
+        intervalId = setInterval(autoSwitchImage, 3000); // 重新開始自動切換
+        btn.textContent = 'Pause';
+    }
+    isPlaying = !isPlaying;
+}
